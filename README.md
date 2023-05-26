5c1fe4c1f6eca3bd9b775c1217a3b2c2aa33da4d
Mike Bell <mbell@landolakes.com>
2023-05-26 14:56:17 -0500

    forked and modified

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
-[![published](https://static.production.devnetcloud.com/codeexchange/assets/images/devnet-published.svg)](https://developer.cisco.com/codeexchange/github/repo/dannywade/net-textorial)
-
-# net-textorial
+# net-streamtorial
 
 ## Introduction
-Originally developed by [@JulioPDX](https://github.com/JulioPDX), this was a simple app showing off the power of a text-based user interface (TUI) and how it can be used with network automation.
+This is a fork of [net-textorial](https://github.com/dannywade/net-textorial), originally developed by [@dannywade](https://github.com/dannywade) and [@JulioPDX](https://github.com/JulioPDX), which is a simple app showing off the power of a text-based user interface (TUI) and how it can be used with network automation. This fork has modified that code to work in a web interface using streamlit. 
 
-With recent changes, it has evolved into a learning tool that shows network engineers the differences between raw (human-friendly) CLI output and parsed (structured) output. The tool will take any 'show' command and produce the raw and parsed outputs (using [ntc-templates](https://github.com/networktocode/ntc-templates) for parsing). This will be valuable for engineers that want to learn and see the differences between raw (unstructured) and parsed (structured) data outputs.
+With recent changes, it has evolved into a learning tool that shows network engineers the differences between raw (human-friendly) CLI output and parsed (structured) output. The tool will take any 'show' command and produce the raw and parsed outputs (using [ntc-templates](https://github.com/networktocode/ntc-templates) for parsing). This will be valuable for engineers that want to learn and see the differences between raw (unstructured) and parsed (structured) data outputs. Additionally, tabular output has been added with the pandas library. 
 
-Under the hood, this tool is leveraging [Netmiko](https://github.com/ktbyers/netmiko) and [Textual](https://textual.textualize.io/). Thank you to all involved!
+Under the hood, this tool is leveraging [Netmiko](https://github.com/ktbyers/netmiko) and [Streamlit](hhttps://streamlit.io/). Thank you to all involved!
 
 If you'd like an overview of the tool, check out my [YouTube video](https://youtu.be/H8uGOIK2ZqY)!
 
@@ -18,8 +16,8 @@ If you'd like an overview of the tool, check out my [YouTube video](https://yout
 ## Get Started
 
 ```shell
-git clone https://github.com/dannywade/net-textual
-cd net-textual
+git clone https://github.com/netbell/net-streamtorial.git
+cd net-streamtorial
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
@@ -27,7 +25,7 @@ pip3 install -r requirements.txt
 
 ## Basic Usage
 
-1. Set your device CLI credentials as environment variables. The variable key names are `NET_TEXT_USER` and `NET_TEXT_PASS`. If device CLI credentials are not set, the default value of *admin* is set for both username and password.
+1. Set your device CLI credentials as environment variables. The variable key names are `NET_TEXT_USER` and `NET_TEXT_PASS`. If device CLI credentials are not set, the default value of *admin* is set for both username and password. This can also be accomplished by creating a .env file with the same values.
 
     ```shell
     NET_TEXT_USER=<your username>
@@ -36,13 +34,11 @@ pip3 install -r requirements.txt
 2. Run the app
 
     ```shell
-    python net.py
+    streamlit run net.py
     ```
-3. Enter the hostname/IP of the device and a valid 'show' command in the input box
+3. Enter the hostname/IP of the device and a valid 'show' command, hit enter or clock 'Run'
 
-    ```shell
-    dist-router1 show ip route
-    ```
+    ![](screenshot-1.png)
 
 ## Supported Devices/Parsers
 
@@ -60,17 +56,8 @@ Any device that is supported by Netmiko can be used with this tool. Under the ho
 
 With Genie and ntc-templates (TextFSM) being the two major parsing libraries in the game, there's a goal to support both libraries. Currently, only ntc-templates is supported and used to parse all command output. Genie parsers will be supported in the future.
 
-### ChatGPT Integration (optional)
-
-If you would like the assistance of ChatGPT to help explain the parsed output, there's an optional integration using [OpenAI's API](https://openai.com/blog/openai-api). All you need to do is set your OpenAI API token as an environment variable and net-textorial will ask ChatGPT to explain the parsed output.
-
-```shell
-    OPEN_AI_KEY=<your token>
-```
-
-***DISCLAIMER: Please make your own interpretation of the ChatGPT results. The results may differ. For more information, reference OpenAI's [terms of use](https://openai.com/policies/terms-of-use).***
 
 ## Demo
 
-https://user-images.githubusercontent.com/13680237/199552401-9e9c58b0-6cd9-4dcb-8512-b91cc8d010b0.mov
+
 
--- net.py
+++ net.py
@@ -1,209 +1,49 @@
-import json
-import os
-import requests
-from rich.syntax import Syntax
-from rich.tree import Tree
-from pathlib import Path
-import pyperclip
-from rich.text import Text
-from textual import work
-from textual.app import App, ComposeResult
-from textual.binding import Binding
-from textual.containers import VerticalScroll, Container
-from textual.widgets import Static, Input, Footer, Button, Tabs
-from textual.worker import get_current_worker
-from textual_autocomplete import AutoComplete, Dropdown
-
-# from textual_autocomplete._autocomplete import AutoComplete, Dropdown
-
-# local imports
-from helpers import get_device_info, add_node, get_items, write_json_file
-from inventory import InventorySidebar, InventoryScreen
+import streamlit as st
+import helpers
+import show_commands
+import pandas as pd
 
 
-class NetTextorialApp(App):
-    """Get info from network device"""
+st.set_page_config(page_title="Net-Streamtorial",
+                   layout="wide", page_icon="🔧",)
 
-    CSS_PATH = "net.css"
-    BINDINGS = [
-        Binding("q", "quit", "Quit"),
-        Binding("r", "copy_output", "Copy output"),
-        Binding("i", "inventory", "Inventory"),
-        Binding("v", "push_screen('inventory')", "Inventory Page"),
-    ]
-    SCREENS = {"inventory": InventoryScreen()}
+topcol1, topcol2 = st.columns(2)
 
-    def action_toggle_sidebar(self) -> None:
-        """Called when user hits 'b' key."""
-        self.show_bar = not self.show_bar
+with topcol1:
+    device = st.text_input('Device Hostname')
+    run = st.button("Run")
 
-    def action_copy_output(self) -> None:
-        """Called when user hits 'r' key. Copies the raw command output."""
-        # Queries for widget that holds raw results
-        raw_output = self.query_one("#output-results")
-        tabs = self.query(Tabs).first()
-        # Figures out whether the 'Parsed Output (tree)' tab is currently active
-        if tabs.validate_active("tab-3"):
-            output = "Manually copy tree output from app."
-        else:
-            # Extracts text from 'Syntax' renderable and copies to clipboard
-            output = str(raw_output.render().code)
-        pyperclip.copy(output)
+with topcol2:
+    # command = st.selectbox('Show Command', options=show_commands.SHOW_COMMANDS) # Use with show_commands.py if wanting to limit options
+    command = st.text_input('Show Command', 'show Inventory')
 
-    def compose(self) -> ComposeResult:
-        yield Container(
-            AutoComplete(
-                Input(
-                    placeholder="Enter device hostname/IP and command: '<hostname/IP> show <command>'",
-                    id="command_input",
-                ),
-                Dropdown(
-                    items=get_items,  # Using a callback to dynamically generate items
-                    id="my-dropdown",
-                ),
-            ),
-            Button(label="Go!", variant="primary", id="run_button"),
-            id="input_container",
-        )
-        yield VerticalScroll(
-            Tabs(
-                "Raw Output",
-                "Parsed Output",
-                "Parsed Output (tree)",
-                "Learn with ChatGPT",
-                id="output-tabs",
-            ),
-            Static(id="output-results", classes="result"),
-            classes="results-container",
-        )
-        yield Footer()
-        self.inventory = InventorySidebar(classes="hidden")
-        yield self.inventory
+tab1, tab2, tab3 = st.tabs(["Raw Output", "Parsed Output", "Table"])
 
-    def on_mount(self) -> None:
-        """Called when app starts."""
-        # Give the input focus, so we can start typing straight away
-        self.query_one("#command_input").focus()
-        # Initialize outputs
-        self.raw_output = ""
-        self.parsed_output = ""
+if run:
+    raw_output, parsed_output = helpers.get_device_info(f"{device} {command}")
 
-    def on_button_pressed(self, _: Button.Pressed) -> None:
-        """Run when user clicks 'Go!' button"""
-        user_input = self.query_one("#command_input")
-        if user_input.value:
-            # Get user input when user clicks 'Go!' button
-            outputs = get_device_info(user_input.value)
-            self.raw_output = outputs[0]
-            self.parsed_output = outputs[1]
-            # Write parsed output to local JSON file
-            write_json_file("parsed_output", self.parsed_output)
+    with tab1:
+        st.header("Raw Output")
+        st.code(raw_output, language="cisco", line_numbers=False)
 
-    def action_inventory(self) -> None:
-        """Toggle the display of the inventory sidebar"""
-        if self.inventory.shown:
-            self.inventory.hide()
-        else:
-            self.inventory.show()
+    with tab2:
+        st.header("Parsed Output")
+        st.code(parsed_output, language="json", line_numbers=False)
 
-    async def on_tabs_tab_activated(self, event: Tabs.TabActivated) -> None:
-        """Handle TabActivated message sent by Tabs."""
-        # Raw Output tab
-        if event.tab.id == "tab-1":
-            self.query_one("#output-results", Static).update(
-                Syntax(
-                    self.raw_output, "teratermmacro", theme="nord", line_numbers=True
-                )
-            )
-        # Parsed Output tab
-        elif event.tab.id == "tab-2":
-            try:
-                # Load the JSON file
-                file_path = Path(__file__).parent / "parsed_output.json"
-                with open(file_path) as parsed_data:
-                    self.parsed_output = json.load(parsed_data)
-            except:
-                self.query_one("#output-results", Static).update(
-                    "Local JSON file could not be loaded. Please ensure parsed output is available."
-                )
-                return
-            # Convert loaded JSON to string for display
-            parsed_jstring = json.dumps(self.parsed_output, indent=2)
-            self.query_one("#output-results", Static).update(
-                Syntax(
-                    parsed_jstring,
-                    "teratermmacro",
-                    theme="nord",
-                    line_numbers=True,
-                )
-            )
-        # Parsed Output (tree) tab
-        elif event.tab.id == "tab-3":
-            # Load the JSON file
-            try:
-                file_path = Path(__file__).parent / "parsed_output.json"
-                with open(file_path) as parsed_data:
-                    self.json_data = json.load(parsed_data)
-            except:
-                self.query_one("#output-results", Static).update(
-                    "Local JSON file could not be loaded. Please ensure parsed output is available."
-                )
-                return
-            # Update the correct tab
-            tree: Tree[dict] = Tree("Parsed Output")
-            # json_node = tree.add("Parsed Output")
-            tree = add_node("Parsed Output", tree, self.json_data)
-            self.query_one("#output-results", Static).update(tree)
-        # Learn with ChatGPT tab
-        elif event.tab.id == "tab-4":
-            # Clear results box and provide useful feedback to user
-            self.query_one("#output-results", Static).update(
-                "Please wait... ChatGPT is analyzing the JSON payload."
-            )
-            # Load the JSON file, if not already loaded
-            try:
-                file_path = Path(__file__).parent / "parsed_output.json"
-                with open(file_path) as parsed_data:
-                    self.json_data = json.load(parsed_data)
-            except:
-                self.query_one("#output-results", Static).update(
-                    "Local JSON file could not be loaded. Please ensure parsed output is available."
-                )
-                return
-            # Ask ChatGPT to analyze JSON
-            self.ai_chat(f"Tell me about this JSON payload: {self.json_data}")
+    with tab3:
+        df = pd.read_json(parsed_output)
+        st.header("Parsed Output")
+        if parsed_output:
+            st.dataframe(df, use_container_width=True, height=1000)
+else:
+    with tab1:
+        st.header("Raw Output")
+        st.code("", language="cisco", line_numbers=False)
 
-    @work(exclusive=True)
-    def ai_chat(self, prompt: str) -> str:
-        """Ask ChatGPT a question. Assumes API key is set as an environment variable"""
-        api_key = os.getenv("OPEN_AI_KEY")
-        worker = get_current_worker()
-        chatgpt_widget = self.query_one("#output-results", Static)
+    with tab2:
+        st.header("Parsed Output")
+        st.code("", language="json", line_numbers=False)
 
-        if prompt and api_key is not None:
-            response = requests.post(
-                url="https://api.openai.com/v1/chat/completions",
-                headers={"authorization": f"Bearer {api_key}"},
-                json={
-                    "model": "gpt-3.5-turbo",
-                    "messages": [{"role": "user", "content": prompt}],
-                },
-            )
-            chatgpt_results = Text.from_ansi(
-                response.json()["choices"][0]["message"]["content"]
-            )
-            if not worker.is_cancelled:
-                # Update widget from thread
-                self.call_from_thread(chatgpt_widget.update, chatgpt_results)
-        else:
-            # No result from ChatGPT, return blank
-            if not worker.is_cancelled:
-                self.call_from_thread(
-                    chatgpt_widget.update,
-                    "Sorry, no OpenAI API key was found. Please make sure to set an environment variable.",
-                )
-
-
-if __name__ == "__main__":
-    app = NetTextorialApp()
-    app.run()
+    with tab3:
+        st.header("Table")
+        st.dataframe(data=None, use_container_width=True)
