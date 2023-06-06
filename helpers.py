37f6113410fb5ea27af4f21f69239de8f5a7b47c
mbell <michaelbell908@gmail.com>
2023-06-06 08:09:28 -0500

    Improved error handling and credentials input

--- helpers.py
+++ helpers.py
@@ -1,106 +1,84 @@
 import ipaddress
 import json
-from netmiko.exceptions import (
-    NetmikoAuthenticationException,
-    NetmikoTimeoutException,
-)
-from netmiko import ConnectHandler
-from netmiko.ssh_autodetect import SSHDetect
 import os
+import streamlit as st
 from dotenv import load_dotenv
+from netmiko import ConnectHandler
+from netmiko.exceptions import NetmikoAuthenticationException, NetmikoTimeoutException
+from netmiko.ssh_autodetect import SSHDetect
 
 load_dotenv()
 
+class NetworkDevice:
+    def __init__(self, host_id: str, credentials: dict, device_type: str):
+        self.host_id = host_id
+        self.credentials = credentials
+        self.device_type = device_type  # device type is set here
+        self.connection = None
 
-def device_connection(host_id: str, credentials: dict) -> ConnectHandler:
-    """
-    Automatically handles device connections using Netmiko.
-
-    Args:
-        host_id (str): Commonly the hostname of a network device
-        credentials (dict): A dictionary with 'username' and 'password' as keys
-
-    Returns:
-        ConnectHandler object
-    """
-    remote_device = {
-        "device_type": "autodetect",
-        "host": host_id,
-        "username": credentials.get("username"),
-        "password": credentials.get("password"),
-    }
-
-    try:
-        guesser = SSHDetect(**remote_device)
-        best_match = guesser.autodetect()
-        remote_device["device_type"] = best_match
-        connection = ConnectHandler(**remote_device)
-    except (
-        NetmikoTimeoutException,
-        NetmikoAuthenticationException,
-    ) as e:
-        print(f"Could not connect to device due to the following error: {e}")
-        connection = None
-
-    return connection
-
-
-def get_device_info(user_input) -> tuple:
-    """
-    Allows user to run any CLI command and have the raw and parsed output returned.
-    """
-
-    command_list = user_input.split(" ")
-    # Check whether entered host is an IP address or hostname
-    # It won't matter now, but can provide simple validation in future.
-    try:
-        ipaddress.ip_address(command_list[0])
-        host = command_list[0]
-    except ValueError:
-        host = command_list[0]
-    host = command_list[0]
-    # Try reading from env vars, default to admin/admin
-    user = os.getenv("NET_TEXT_USER", "admin")
-    pw = os.getenv("NET_TEXT_PASS", "admin")
-    creds = {"username": user, "password": pw}
-    dev_connect = device_connection(host_id=host, credentials=creds)
-    if dev_connect is not None:
+    def device_connection(self):
+        remote_device = {
+            "device_type": self.device_type,
+            "host": self.host_id,
+            "username": self.credentials.get("username"),
+            "password": self.credentials.get("password"),
+        }
+        if self.device_type == "autodetect":
+            try:
+                guesser = SSHDetect(**remote_device)
+                best_match = guesser.autodetect()
+                remote_device["device_type"] = best_match
+            except NetmikoTimeoutException:
+                st.exception("The connection to the device timed out.")
+                self.connection = None
+            except NetmikoAuthenticationException as e:
+                st.exception(e)
+                self.connection = None
         try:
-            if command_list[1] != "show":
-                raise Exception("Only 'show' commands are supported.")
-            else:
-                with dev_connect as device:
-                    raw_output = device.send_command((" ".join(command_list[1:])))
-                    parsed_output = device.send_command(
-                        (" ".join(command_list[1:])), use_textfsm=True
-                    )
+            self.connection = ConnectHandler(**remote_device)
+        except NetmikoTimeoutException:
+            st.exception("The connection to the device timed out.")
+            self.connection = None
+        except NetmikoAuthenticationException as e:
+            st.exception(e)
+            self.connection = None
+
+    def get_device_info(self, user_input):
+        command_list = user_input.split(" ")
+        try:
+            ipaddress.ip_address(command_list[0])
+            host = command_list[0]
+        except ValueError:
+            host = command_list[0]
+
+        if self.connection is not None:
+            try:
+                if command_list[1] != "show":
+                    raise Exception("Only 'show' commands are supported.")
+                else:
+                    raw_output = self.connection.send_command((" ".join(command_list[1:])))
+                    parsed_output = self.connection.send_command((" ".join(command_list[1:])), use_textfsm=True)
                     if not parsed_output:
                         parsed_output = "N/A"
-        except (NetmikoTimeoutException, NetmikoAuthenticationException) as e:
-            raw_output = f"There was an issue connecting to the device: {e}"
-            parsed_output = "N/A"
-        except Exception as e:
-            raw_output = f"There was an error: {e}"
-            parsed_output = "N/A"
-    else:
-        raw_output = "Could not connect to device."
-        parsed_output = "N/A"
+            except (NetmikoTimeoutException, NetmikoAuthenticationException, Exception) as e:
+                st.exception(e)
+                raw_output = None
+                parsed_output = None
+        else:
+            raw_output = None
+            parsed_output = None
 
-    # Cleanse the output if invalid command provided by user
-    if "Invalid input detected" in raw_output:
-        raw_output = "Invalid command sent to the device."
-        parsed_output = "No parser available."
-    # Convert parsed output to string and format
-    if "\n" in parsed_output:
-        # If newline characters are returned, there's a good chance there was not a parser available
-        parsed_output = "No parser available."
-    else:
-        try:
-            # Check if parsed_output is an iterable (dict, list, etc.) and convert to JSON string
-            iter(parsed_output)
-            parsed_output = json.dumps(parsed_output, indent=2)
-        except TypeError:
-            # parsed_output is not an iterable (most likely a string), so JSON string conversion is not necessary
-            pass
+        if raw_output and ("invalid input" in raw_output.lower() or "invalid command" in raw_output.lower()):
+            raw_output = "Invalid command sent to the device."
+            parsed_output = None
 
-    return raw_output, parsed_output
+        if parsed_output and "\n" in parsed_output:
+            parsed_output = None
+        else:
+            try:
+                iter(parsed_output)
+                parsed_output = json.dumps(parsed_output, indent=2)
+            except TypeError:
+                pass
+
+        return raw_output, parsed_output
--- net.py
+++ net.py
@@ -1,40 +1,88 @@
 import streamlit as st
-import helpers
-import show_commands
 import pandas as pd
-
+from dotenv import load_dotenv
+import os
+from helpers import NetworkDevice
 
 st.set_page_config(page_title="Net-Streamtorial",
-                   layout="wide", page_icon="🔧",)
+                   layout="wide", page_icon="🔧")
 
-topcol1, topcol2 = st.columns(2)
+# Load the environmental variables
+load_dotenv()
+
+# Check for environment variables. If not present, use Streamlit to get the inputs.
+user = os.getenv("NET_TEXT_USER")
+pw = os.getenv("NET_TEXT_PASS")
+
+
+if not user and pw:
+        with st.expander("Credentials", expanded=True):
+            user = st.text_input("Username")
+            pw = st.text_input("Password", type="password")
+
+creds = {"username": user, "password": pw}
+
+platforms = {
+    "Arista EOS": "arista_eos",
+    "Cisco ASA": "cisco_asa",
+    "Cisco IOS": "cisco_ios",
+    "Cisco NX-OS": "cisco_nxos",
+    "Cisco IOS XE": "cisco_xe",
+    "Cisco IOS XR": "cisco_xr",
+    "Juniper Junos": "juniper_junos",
+    "Autodetect": "autodetect"
+}
+
+topcol1, topcol2, topcol3 = st.columns(3)
 
 with topcol1:
     device = st.text_input('Device Hostname')
     run = st.button("Run")
 
 with topcol2:
-    # command = st.selectbox('Show Command', options=show_commands.SHOW_COMMANDS) # Use with show_commands.py if wanting to limit options
     command = st.text_input('Show Command', 'show Inventory')
+
+with topcol3:
+    platform = st.selectbox('Platform', options=list(platforms.keys()), index=len(platforms) - 1)  # default to "Autodetect"
 
 tab1, tab2, tab3 = st.tabs(["Raw Output", "Parsed Output", "Table"])
 
 if run:
-    raw_output, parsed_output = helpers.get_device_info(f"{device} {command}")
+
+    # Create the network device object and establish the connection
+    with st.spinner('Connecting to device...'):
+        # Get the actual platform string from the dictionary
+        platform_type = platforms[platform]
+
+        # Create the network device object and establish the connection
+        network_device = NetworkDevice(device, creds, platform_type)
+        network_device.device_connection()
+
+    # Execute the command and get the output
+    with st.spinner('Executing command...'):
+        raw_output, parsed_output = network_device.get_device_info(f"{device} {command}")
 
     with tab1:
         st.header("Raw Output")
-        st.code(raw_output, language="cisco", line_numbers=False)
+        if raw_output is None:
+            st.warning("No data to display.")
+        else:
+            st.code(raw_output, language="cisco", line_numbers=False)
 
     with tab2:
         st.header("Parsed Output")
-        st.code(parsed_output, language="json", line_numbers=False)
+        if parsed_output is None:
+            st.warning("No data to display.")
+        else:    
+            st.code(parsed_output, language="json", line_numbers=False)
 
     with tab3:
-        df = pd.read_json(parsed_output)
         st.header("Table")
-        if parsed_output:
+        try:
+            df = pd.read_json(parsed_output)
             st.dataframe(df, use_container_width=True, height=1000)
+        except ValueError:
+            st.warning("No data to display")
 else:
     with tab1:
         st.header("Raw Output")
